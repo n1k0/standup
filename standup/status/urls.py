10a85e44c226a908cb655cc6b6f730d233d73b73
Nicolas Perriault <nicolas@perriault.net>
2016-12-27 13:32:45 +0100

    Specialcase username slugs.

--- standup/status/urls.py
+++ standup/status/urls.py
@@ -5,14 +5,15 @@ from django.views.decorators.cache import cache_page
 from . import views
 
 
-SLUG_RE = r'(?P<slug>[-a-zA-Z0-9_@`\[\]\{\}\^]+)'
+SLUG_RE = r'(?P<slug>[-a-zA-Z0-9_@]+)'
+USERNAME_RE = r'(?P<slug>[-a-zA-Z0-9_@`\[\]\{\}\^]+)'
 cache_feed = cache_page(settings.CACHE_FEEDS_SECONDS)
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='status.index'),
     url(r'^team/{}/$'.format(SLUG_RE), views.TeamView.as_view(), name='status.team'),
     url(r'^project/{}/$'.format(SLUG_RE), views.ProjectView.as_view(), name='status.project'),
-    url(r'^user/{}/$'.format(SLUG_RE), views.UserView.as_view(), name='status.user'),
+    url(r'^user/{}/$'.format(USERNAME_RE), views.UserView.as_view(), name='status.user'),
     url(r'^status/(?P<pk>\d{1,8})/$', views.StatusView.as_view(), name='status.status'),
     url(r'^weekly/$', views.WeeklyView.as_view(), name='status.weekly'),
     url(r'^statusize/$', views.statusize, name='status.statusize'),
@@ -25,7 +26,7 @@ urlpatterns = [
 
     # feeds
     url(r'^statuses.xml$', cache_feed(views.MainFeed()), name='status.index_feed'),
-    url(r'^user/{}.xml$'.format(SLUG_RE), cache_feed(views.UserFeed()), name='status.user_feed'),
+    url(r'^user/{}.xml$'.format(USERNAME_RE), cache_feed(views.UserFeed()), name='status.user_feed'),
     url(r'^team/{}.xml$'.format(SLUG_RE), cache_feed(views.TeamFeed()), name='status.team_feed'),
     url(r'^project/{}.xml$'.format(SLUG_RE), cache_feed(views.ProjectFeed()),
         name='status.project_feed'),
